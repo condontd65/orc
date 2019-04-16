b3fe049a4c77c8a174765dceeaf2c0f054d5cc67
Timothy Condon <151268@cityhall.boston.cob>
2019-04-16 16:58:51 -0400

    Change method of getting dates out. Don't convert to date as it will just be a string in the end and converting to date has some issues

--- clean_intake_data.R
+++ clean_intake_data.R
@@ -5,6 +5,8 @@ library(stringdist)
 library(dplyr)
 library(googlesheets)
 library(anytime)
+library(tidyr)
+library(stringr)
 
 ##### Input Data and Begin Clean #####
 intake <- read.csv('tables/Office_of_Returning_Citizens_Intake_Form_2019-02-06_145515_bec13ce6.csv') %>%
@@ -64,7 +66,8 @@ intake.true <- intake.ordered[ intake.ordered$dup == TRUE ]
 intake.deduped <- intake.ordered[ intake.ordered$dup == FALSE ]
 
 
-##### Fix Dates ##### 
+##### Bad Fix Dates ##### 
+.comment <- function(){
 # String manipulation to clean up start dates
 start.date <- intake.deduped$Start.date.of.your.most.recent.period.of.Incarceration.
 start.date <- gsub('   ', '', start.date)
@@ -81,7 +84,8 @@ start.date <- ifelse(substr(start.date, 1, 1) == '-', sub("^.", "", start.date),
 start.date <- ifelse(substrRight(start.date, 1) == '-', sub(".$", "", start.date), start.date)
 
 # Format longer strings as dates "%m-%d-%Y"
-start.date.t1 <- ifelse(nchar(start.date) > 7, as.Date(start.date,format = "%m-%d-%Y"), start.date)
+start.date.t <- gsub('-','/',start.date)
+start.date.t1 <- ifelse(nchar(start.date.t) > 7, as.Date(start.date.t,format = "%m/%d/%Y"),start.date.t)
 
 
 
@@ -90,8 +94,219 @@ eval <- data.frame(test, test2, test3, test4)
 
 test <- as.Date(test, format = "%m %d %Y")
 test <- gsub(' ', '-', test)
+}
+
+##### Fix Dates #####
+
+# Create new dataframe from intake.deduped to allow more analysis
+it <- intake.deduped
+
+# Use separate to split months, days, years
+it <- separate(it, col = Start.date.of.your.most.recent.period.of.Incarceration., 
+         into = c('start.month','start.day','start.year'), 
+         sep = ' ')
+
+# Change NA to nothing
+it$start.day [ is.na(it$start.day) ] <- ''
+it$start.month [ is.na(it$start.month) ] <- ''
+it$start.year [ is.na(it$start.year) ] <- ''
+
+# Pad day and month to add leading zeros
+it$start.day <- ifelse(nchar(it$start.day) == 1,
+                       str_pad(it$start.day,
+                               width = 2,
+                               side = 'left',
+                               pad = '0'),
+                       it$start.day)
+it$start.month <- ifelse(nchar(it$start.month) == 1,
+                       str_pad(it$start.month,
+                               width = 2,
+                               side = 'left',
+                               pad = '0'),
+                       it$start.month)
+
+# Combine all into a date,seperated by "-"
+it$Start.date.of.your.most.recent.period.of.Incarceration. <- paste(it$start.month,
+                                                                                it$start.day,
+                                                                                it$start.year,
+                                                                    sep = "-")
+
+# Remove starting "-"                                                                                                                                                    sep = "-")
+it$Start.date.of.your.most.recent.period.of.Incarceration. <- ifelse(substr(it$Start.date.of.your.most.recent.period.of.Incarceration., 1, 1) == '-', 
+                                                                     sub("^.", "", it$Start.date.of.your.most.recent.period.of.Incarceration.), 
+                                                                     it$Start.date.of.your.most.recent.period.of.Incarceration.)
+# Remove ending "-"
+it$Start.date.of.your.most.recent.period.of.Incarceration. <- ifelse(substrRight(it$Start.date.of.your.most.recent.period.of.Incarceration., 1) == '-', 
+                                                                     sub(".$", "", it$Start.date.of.your.most.recent.period.of.Incarceration.), 
+                                                                     it$Start.date.of.your.most.recent.period.of.Incarceration.)
+# Remove ending "-"
+it$Start.date.of.your.most.recent.period.of.Incarceration. <- ifelse(substrRight(it$Start.date.of.your.most.recent.period.of.Incarceration., 1) == '-', 
+                                                                     sub(".$", "", it$Start.date.of.your.most.recent.period.of.Incarceration.), 
+                                                                     it$Start.date.of.your.most.recent.period.of.Incarceration.)
+
+## Now do the same thing for end date
+it <- separate(it, col = End.date.of.your.most.recent.period.of.Incarceration., 
+               into = c('end.month','end.day','end.year'), 
+               sep = ' ')
+
+# Change NA to nothing
+it$end.day [ is.na(it$end.day) ] <- ''
+it$end.month [ is.na(it$end.month) ] <- ''
+it$end.year [ is.na(it$end.year) ] <- ''
+
+# Pad day and month to add leading zeros
+it$end.day <- ifelse(nchar(it$end.day) == 1,
+                       str_pad(it$end.day,
+                               width = 2,
+                               side = 'left',
+                               pad = '0'),
+                       it$end.day)
+it$end.month <- ifelse(nchar(it$end.month) == 1,
+                         str_pad(it$end.month,
+                                 width = 2,
+                                 side = 'left',
+                                 pad = '0'),
+                         it$end.month)
+
+# Combine all into a date,seperated by "-"
+it$End.date.of.your.most.recent.period.of.Incarceration. <- paste(it$end.month,
+                                                                    it$end.day,
+                                                                    it$end.year,
+                                                                    sep = "-")
+
+# Remove Ending "-"                                                                                                                                                    sep = "-")
+it$End.date.of.your.most.recent.period.of.Incarceration. <- ifelse(substr(it$End.date.of.your.most.recent.period.of.Incarceration., 1, 1) == '-', 
+                                                                     sub("^.", "", it$End.date.of.your.most.recent.period.of.Incarceration.), 
+                                                                     it$End.date.of.your.most.recent.period.of.Incarceration.)
+# Remove ending "-"
+it$End.date.of.your.most.recent.period.of.Incarceration. <- ifelse(substrRight(it$End.date.of.your.most.recent.period.of.Incarceration., 1) == '-', 
+                                                                     sub(".$", "", it$End.date.of.your.most.recent.period.of.Incarceration.), 
+                                                                     it$End.date.of.your.most.recent.period.of.Incarceration.)
+# Remove ending "-"
+it$End.date.of.your.most.recent.period.of.Incarceration. <- ifelse(substrRight(it$End.date.of.your.most.recent.period.of.Incarceration., 1) == '-', 
+                                                                     sub(".$", "", it$End.date.of.your.most.recent.period.of.Incarceration.), 
+                                                                     it$End.date.of.your.most.recent.period.of.Incarceration.)
+
+
+## Now do the same thing for date completed
+it <- separate(it, col = Date.completed., 
+               into = c('month','day','year'), 
+               sep = ' ')
+
+# Change NA to nothing
+it$day [ is.na(it$day) ] <- ''
+it$month [ is.na(it$month) ] <- ''
+it$year [ is.na(it$year) ] <- ''
+
+# Pad day and month to add leading zeros
+it$day <- ifelse(nchar(it$day) == 1,
+                     str_pad(it$day,
+                             width = 2,
+                             side = 'left',
+                             pad = '0'),
+                     it$day)
+it$month <- ifelse(nchar(it$month) == 1,
+                       str_pad(it$month,
+                               width = 2,
+                               side = 'left',
+                               pad = '0'),
+                       it$month)
+
+# Combine all into a date,seperated by "-"
+it$Date.completed. <- paste(it$month,
+                            it$day,
+                            it$year,
+                            sep = "-")
+
+# Remove Ending "-"                                                                                                                                                    sep = "-")
+it$Date.completed. <- ifelse(substr(it$Date.completed., 1, 1) == '-', 
+                                                                   sub("^.", "", it$Date.completed.), 
+                                                                   it$Date.completed.)
+
+it$Date.completed. <- ifelse(substr(it$Date.completed., 1, 1) == '-', 
+                             sub("^.", "", it$Date.completed.), 
+                             it$Date.completed.)
+# Remove ending "-"
+it$Date.completed. <- ifelse(substrRight(it$Date.completed., 1) == '-', 
+                                                                   sub(".$", "", it$Date.completed.), 
+                                                                   it$Date.completed.)
+# Remove ending "-"
+it$Date.completed. <- ifelse(substrRight(it$Date.completed., 1) == '-', 
+                                                                   sub(".$", "", it$Date.completed.), 
+                                                                   it$Date.completed.)
+it$Date.completed. <- gsub('--','-',it$Date.completed.)
+
+
+
+# Drop dataframe columns that are unecessary
+
+
+
+
+
+
+
+
+
+#it$Start.date.of.your.most.recent.period.of.Incarceration. <- ifelse(nchar(it$Start.date.of.your.most.recent.period.of.Incarceration.) == 10, 
+#                       as.Date(it$Start.date.of.your.most.recent.period.of.Incarceration., format = "%Y-%m-%d"),
+#                       it$Start.date.of.your.most.recent.period.of.Incarceration.)
+
+
+ 
+
+#  
+intake.deduped$End.date.of.your.most.recent.period.of.Incarceration.
+
+
+
+
+
+
+
+
 
 
 
 # Plot in window
 plot(gvisTable(dupes))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Random prk stuff
+old.count <- 12302
+new.count <- 19659
+dif <- new.count - old.count
+
+old.area <- 562393464
+new.area <- 737097723
+dif <- new.area - old.area
+dif.miles <- dif / 27880000
+
+rm(old.count,new.count,dif,old.area,new.area,dif.miles)
+
+
+
+
+
+
+
+
+
+
